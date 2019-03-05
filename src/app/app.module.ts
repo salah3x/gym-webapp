f5eb97b7283092480258fb14155fefbe3e23e36a
salah3x <salah.loukili@gmail.com>
2019-03-06 00:06:27 +0100

    Add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {AngularFireModule} from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './shared/app-material.module';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import { AppMaterialModule } from './shared/app-material.module';
     BrowserAnimationsModule,
     FlexLayoutModule,
     AppMaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCg-szSoxtGNZR1NGdGr02jDzhYGP1COZ4',
+    authDomain: 'gym-webapp.firebaseapp.com',
+    databaseURL: 'https://gym-webapp.firebaseio.com',
+    projectId: 'gym-webapp',
+    storageBucket: 'gym-webapp.appspot.com',
+    messagingSenderId: '217921355428'
+  }
 };
 
 /*
