d47980e4a3c7633f78b7805d709f1e962a6edc48
salah3x <salah.loukili@gmail.com>
2019-03-08 00:25:38 +0100

    fix express serving endpoint and add body-parser

--- functions/src/app/super-admin.ts
+++ functions/src/app/super-admin.ts
@@ -1,16 +1,19 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 export const app: express.Application = express();
-
 function isSuperAdminMiddleware(request: express.Request,
                                 response: express.Response,
                                 next: express.NextFunction) {
     // todo: verify if the current user has superAdmin claim, if not return 403 error.
     next();
 }
-
 app.use(isSuperAdminMiddleware);
+app.use(bodyParser.json());
+const route: express.Router = express.Router()
 
-app.get('/hello', (req: express.Request, res: express.Response) => {
+route.get('/hello', (req: express.Request, res: express.Response) => {
     res.json({message: 'Hello World!'})
 })
+
+app.use('/api/superAdmin', route);
