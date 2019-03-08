09ff1f0998c6c542445140696469ed97878a4c2f
salah3x <salah.loukili@gmail.com>
2019-03-08 17:00:45 +0100

    Initialize firebase admin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
 import * as superAdminApp from './app/super-admin';
+
+admin.initializeApp(functions.config().firebase);
 
 export const superAdmin = functions.https.onRequest(superAdminApp.app);
