7d7e63efd0ed359ae4f0576a35766b5f3d3df5bc
salah3x <salah.loukili@gmail.com>
2019-03-12 18:24:40 +0100

    ugly fix for #10 :(

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit, ViewEncapsulation, Renderer2, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component, OnInit, OnDestroy, ViewEncapsulation, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit() {
+    (this.renderer.selectRootElement('mat-sidenav-content', true) as HTMLElement)
+      .style.backgroundImage = 'url("http://www.howleysbutchers.ie/wp-content/uploads/2016/03/empty-gym.jpg")';
+    (this.renderer.selectRootElement('mat-sidenav-content', true) as HTMLElement)
+      .style.backgroundPosition = 'center';
+    (this.renderer.selectRootElement('mat-sidenav-content', true) as HTMLElement)
+      .style.backgroundSize = 'cover';
+    (this.renderer.selectRootElement('.mat-toolbar.mat-primary', true) as HTMLElement)
+    .style.background = '#00000090';
   }
 
+  ngOnDestroy() {
+    (this.renderer.selectRootElement('mat-sidenav-content', true) as HTMLElement)
+      .style.backgroundImage = '';
+    (this.renderer.selectRootElement('.mat-toolbar.mat-primary', true) as HTMLElement)
+      .style.background = '#3f51b5';
+  }
 }
