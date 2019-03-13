92db7cdc2408334baeca9e13d7b56b04343d0cdb
salah3x <salah.loukili@gmail.com>
2019-03-13 18:30:26 +0100

    Refactor error handling for superadmin function

--- functions/src/app/super-admin.ts
+++ functions/src/app/super-admin.ts
@@ -3,18 +3,24 @@ import * as admin from 'firebase-admin'
 
 // Initialize an express app
 export const app: express.Application = express();
-// Add a middleware to verify caller permissions (expect caller uid in request's body)
+// Add a middleware to verify caller permissions (expect caller token in request's body)
 async function isSuperAdminMiddleware(req: express.Request,
                                     res: express.Response,
                                     next: express.NextFunction) {
-    const uid = req.body.uid;
-    if (!uid) {
-        res.status(401);
+    const token = req.body.token;
+    if (!token) {
+        res.status(400).send({"error": "Bad request", "message": "Token must not be null"});
         return;
     }
-    const claims = await admin.auth().verifyIdToken(uid);
+    let claims;
+    try {
+        claims = await admin.auth().verifyIdToken(token);
+    } catch(err) {
+        res.status(401).send({"error": "Forbidden", "message": "Token rejected"});
+        return;
+    }
     if (!claims || !claims.superadmin) {
-        res.sendStatus(403);
+        res.status(403).send({"error": "Unauthorized", "message": "You do not have required permission"});;
         return;
     }
     next();
@@ -26,17 +32,22 @@ route.post('/updateClaims', async (req: express.Request, res: express.Response) 
     const email = req.body.email;
     const claims = req.body.claims;
     if (!email || !claims) {
-        res.status(400).send('email and claims must not be null.')
+        res.status(400).send({"error": "Bad request", "message": "Email and claims must not be null"})
+        return
     }
-    const user = await admin.auth().getUserByEmail(email);
+    let user;
+    try {
+        user = await admin.auth().getUserByEmail(email);
+    } catch (err) {
+        res.status(500).send({"error": "Internal error", "message": "Retrieving user by email failed"})
+        return;
+    }
     return admin.auth().setCustomUserClaims(user.uid, claims)
                 .then(() => {
-                    console.log(`Setting claims to ${email}: `, claims)
-                    res.sendStatus(201);
+                    res.status(202).send({"message": `Claims have been set to ${email} successfully`});
                 })
                 .catch((error) => {
-                    console.error(`Setting claims to ${email} failed: `, error);
-                    res.sendStatus(500);
+                    res.status(500).send({"error": "Internal error", "message": `Setting claims to ${email} failed`});
                 });
 });
 app.use('/api/superadmin', route);
