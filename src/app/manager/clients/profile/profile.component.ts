a8725ff60c63d800e93dd10f430134f665d53e24
salah3x <salah.loukili@gmail.com>
2019-03-28 23:06:04 +0100

    Add payments section in lient's profile closes #15

--- src/app/manager/clients/profile/profile.component.ts
+++ src/app/manager/clients/profile/profile.component.ts
@@ -4,9 +4,10 @@ import { ActivatedRoute } from '@angular/router';
 import { MatDialog, MatSnackBar } from '@angular/material';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { map } from 'rxjs/operators';
+import { Observable, combineLatest } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
-import { Client, ClientWithId, Pack, Subscription } from 'src/app/shared/client.model';
+import { Client, ClientWithId, Pack, Subscription, Payment } from 'src/app/shared/client.model';
 import { PaymentAddComponent } from './payment-add/payment-add.component';
 import { ClientService } from '../client-service.service';
 
@@ -21,6 +22,8 @@ export class ProfileComponent implements OnInit {
   isLoading = true;
   isCheckingIn = false;
   isAdmin = false;
+  payments: Observable<Payment[]>
+  displayedColumns = ['date', 'price', 'note', 'by'];
 
   constructor(private route: ActivatedRoute,
               private afs: AngularFirestore,
@@ -33,49 +36,41 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.auth.idTokenResult.subscribe(r => r ? r.claims ? this.isAdmin = r.claims.admin : false : false);
     const id = this.route.snapshot.params.id;
-    this.afs.doc<Client>(`clients/${id}`).valueChanges()
-      .pipe(
-        // add a url field with an observable of dawnload url
-        // add a pack field with an observable of the pack object
-        // add a payed field
-        // add a subscription field with an observable of the subscription object
-        map(c => {
-          (c as any).url = this.service.getUrl(c.photo, c.sex);
-          (c.pack as any).pack = this.afs.doc<Pack>(`packs/${c.pack.idPack}`).valueChanges();
-          (c as any).payed = this.service.hasPayed(c.pack.idSubscription);
-          (c.pack as any).subscription = this.afs.doc<Subscription>(`packs/${c.pack.idPack}/subscriptions/${c.pack.idSubscription}`)
-            .valueChanges();
-          return c;
-        })
-      ).subscribe(
-        c => { this.isLoading = false; this.client = { ...c, id }; },
-        () => { this.isLoading = false; this.snack.open('Connexion failed', 'Close', { duration: 3000 }); }
-      );
-    // Get checkins and payments of this client (for the past past year)
-    // payments are either made by the client or by someone else in the same subscription
-    // const startDate = new Date();
-    // startDate.setFullYear(startDate.getFullYear() - 1);
-    // const timestamp = firestore.Timestamp.fromDate(startDate);
-    // this.payments = this.afs.doc<Client>(`clients/${id}`).valueChanges().pipe(
-    //   switchMap(c => {
-    //     return combineLatest(
-    //       this.afs.collection<Payment>('payments', ref => ref
-    //         .where('idClient', '==', id)
-    //         .where('date', '>=', timestamp)
-    //       ).valueChanges(),
-    //       this.afs.collection<Payment>('payments', ref => ref
-    //         .where('idSubscription', '==', c.pack.idSubscription)
-    //         .where('date', '>=', timestamp)
-    //       ).valueChanges()
-    //     );
-    //   }),
-    //   map(list => {
-    //     const [first, second] = list;
-    //     return first.concat(second);
-    //   }),
-    //   map(array => Array.from(new Set(array.map(c => c.date.seconds)))
-    //     .map(seconds => array.find(c => c.date.seconds === seconds))
-    //   ));
+    this.afs.doc<Client>(`clients/${id}`).valueChanges().pipe(
+      // add a url field with an observable of dawnload url
+      // add a payed field (if the client has payed this month's fee)
+      // add a pack field with an observable of the pack object
+      // add a subscription field with an observable of the subscription object
+      map(c => {
+        (c as any).url = this.service.getUrl(c.photo, c.sex);
+        (c as any).payed = this.service.hasPayed(c.pack.idSubscription);
+        (c.pack as any).pack = this.afs.doc<Pack>(`packs/${c.pack.idPack}`).valueChanges();
+        (c.pack as any).subscription = this.afs.doc<Subscription>(`packs/${c.pack.idPack}/subscriptions/${c.pack.idSubscription}`).valueChanges();
+        return c;
+      }),
+      // Get all payments of this client (for the past year)
+      // payments are either made by the client or by someone else in the same subscription
+      tap(c => {
+        if (this.payments) {
+          return;
+        }
+        this.payments = combineLatest(
+          this.afs.collection<Payment>('payments', ref => ref.where('idClient', '==', id)).valueChanges(),
+          this.afs.collection<Payment>('payments', ref => ref.where('idSubscription', '==', c.pack.idSubscription)).valueChanges()
+        ).pipe(
+          map(list => {
+            const [first, second] = list;
+            return first.concat(second);
+          }),
+          map(array => Array.from(new Set(array.map(c => c.date.seconds)))
+            .map(seconds => array.find(c => c.date.seconds === seconds))
+          )
+        );
+      })
+    ).subscribe(
+      c => { this.isLoading = false; this.client = { ...c, id }; },
+      () => { this.isLoading = false; this.snack.open('Connexion failed', 'Close', { duration: 3000 }); }
+    );
   }
 
   openPaymentDialog() {
