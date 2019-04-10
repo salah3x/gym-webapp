c5fed8b38cb453c363d11b7e602cc6f9c3f45e73
salah3x <salah.loukili@gmail.com>
2019-04-11 00:11:10 +0200

    translate main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import {AngularFireAuth} from '@angular/fire/auth';
 import { MatDialog, MatSnackBar, MatSidenav } from '@angular/material';
@@ -15,6 +15,7 @@ export class AppComponent implements OnInit {
   isAuthenticated: boolean;
   claims: any;
   @ViewChild('sidenav') public sideNav: MatSidenav;
+  @ViewChild('i18n') public i18n: ElementRef;
 
   constructor(public router: Router,
               public auth: AngularFireAuth,
@@ -32,8 +33,8 @@ export class AppComponent implements OnInit {
 
   onSigninOrSignout() {
     if (this.isAuthenticated) {
-      this.auth.auth.signOut().then(value => {
-        this.snackBar.open('You have successfully disconnected.', 'Close', {duration: 3000});
+      this.auth.auth.signOut().then(() => {
+        this.snackBar.open(this.i18n.nativeElement.childNodes[0].textContent, 'X', {duration: 3000});
         this.router.navigate(['']);
       });
     } else {
