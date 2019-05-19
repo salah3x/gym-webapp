2d1db55c59bde3fe1624b098e934e6773e14304f
salah3x <salah.loukili@gmail.com>
2019-05-19 16:30:05 +0000

    Add ':f' & ':m' query search in manager module. Closes #80

--- src/app/manager/clients/clients.component.ts
+++ src/app/manager/clients/clients.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Subject, of, combineLatest } from 'rxjs';
 import { switchMap, map, debounceTime, tap, takeUntil } from 'rxjs/operators';
 
-import { ClientWithId, Client } from 'src/app/shared/client.model';
+import { ClientWithId, Client } from '../../shared/client.model';
 import { ClientService } from './client-service.service';
 
 @Component({
@@ -13,7 +19,6 @@ import { ClientService } from './client-service.service';
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit, OnDestroy {
-
   @ViewChild('i18n') public i18n: ElementRef;
   searchTerm = new Subject<string>();
   clients: ClientWithId[];
@@ -22,67 +27,124 @@ export class ClientsComponent implements OnInit, OnDestroy {
   isCheckingIn = false;
   private ngUnsubscribe = new Subject();
 
-  constructor(private afs: AngularFirestore,
-              private snack: MatSnackBar,
-              private service: ClientService) { }
+  constructor(
+    private afs: AngularFirestore,
+    private snack: MatSnackBar,
+    private service: ClientService
+  ) {}
 
   ngOnInit() {
     this.service.i18n = this.i18n;
-    this.searchTerm.pipe(
-      tap(s => s.trim() ? this.isLoading = true : this.isLoading = false),
-      debounceTime(1000),
-      switchMap(s => {
-        if (!s.trim()) {
-          return of([[], [], []]);
-        }
-        if (s === ':all') {
+    this.searchTerm
+      .pipe(
+        tap(s =>
+          s.trim() ? (this.isLoading = true) : (this.isLoading = false)
+        ),
+        debounceTime(1000),
+        switchMap(s => {
+          if (!s.trim()) {
+            return of([[], [], []]);
+          }
+          if (s === ':all') {
+            return combineLatest(
+              this.afs
+                .collection<Client>('clients', ref =>
+                  ref.orderBy('name.first_lowercase')
+                )
+                .snapshotChanges(),
+              of([]),
+              of([])
+            );
+          }
+          if (s === ':f') {
+            return combineLatest(
+              this.afs
+                .collection<Client>('clients', ref =>
+                  ref.where('sex', '==', 'f').orderBy('name.first_lowercase')
+                )
+                .snapshotChanges(),
+              of([]),
+              of([])
+            );
+          }
+          if (s === ':m') {
+            return combineLatest(
+              this.afs
+                .collection<Client>('clients', ref =>
+                  ref.where('sex', '==', 'm').orderBy('name.first_lowercase')
+                )
+                .snapshotChanges(),
+              of([]),
+              of([])
+            );
+          }
+          const firstNameRef = this.afs.collection<Client>('clients', ref =>
+            ref
+              .orderBy('name.first_lowercase')
+              .startAt(s)
+              .endAt(s + '\uf8ff')
+          );
+          const lastNameRef = this.afs.collection<Client>('clients', ref =>
+            ref
+              .orderBy('name.last_lowercase')
+              .startAt(s)
+              .endAt(s + '\uf8ff')
+          );
+          const cinRef = this.afs.collection<Client>('clients', ref =>
+            ref
+              .orderBy('cin')
+              .startAt(s)
+              .endAt(s + '\uf8ff')
+          );
           return combineLatest(
-            this.afs.collection<Client>('clients', ref => ref.orderBy('name.first_lowercase')).snapshotChanges(),
-            of([]),
-            of([]));
+            firstNameRef.snapshotChanges(),
+            lastNameRef.snapshotChanges(),
+            cinRef.snapshotChanges()
+          );
+        }),
+        switchMap(clients => {
+          const [firstNames, lastName, cin] = clients;
+          const combined = firstNames.concat(lastName, cin);
+          return of(combined);
+        }),
+        map(actions =>
+          actions.map(a => {
+            const data = a.payload.doc.data() as Client;
+            const id = a.payload.doc.id;
+            return { id, ...data } as ClientWithId;
+          })
+        ),
+        map(
+          array =>
+            Array.from(new Set(array.map(c => c.id))).map(id =>
+              array.find(c => c.id === id)
+            )
+          // .sort((a, b) => a.name.first.localeCompare(b.name.first))
+        ),
+        map(data => {
+          data.map(c => {
+            // Add .url & .payed as placeholders for some observables
+            (c as any).url = this.service.getUrl(c.photo, c.sex);
+            (c as any).payed = this.service.hasPayed(c.pack.idSubscription);
+          });
+          return data;
+        }),
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe(
+        data => {
+          this.clients = data;
+          this.isLoading = false;
+        },
+        () => {
+          this.snack.open(
+            this.i18n.nativeElement.childNodes[0].textContent,
+            'X',
+            { duration: 2000 }
+          );
+          this.isLoading = false;
         }
-        const firstNameRef = this.afs.collection<Client>('clients', ref => ref.orderBy('name.first_lowercase')
-          .startAt(s).endAt(s + '\uf8ff'));
-        const lastNameRef = this.afs.collection<Client>('clients', ref => ref.orderBy('name.last_lowercase')
-          .startAt(s).endAt(s + '\uf8ff'));
-        const cinRef = this.afs.collection<Client>('clients', ref => ref.orderBy('cin')
-          .startAt(s).endAt(s + '\uf8ff'));
-        return combineLatest(firstNameRef.snapshotChanges(),
-          lastNameRef.snapshotChanges(),
-          cinRef.snapshotChanges());
-      }),
-      switchMap(clients => {
-        const [firstNames, lastName, cin] = clients;
-        const combined = firstNames.concat(lastName, cin);
-        return of(combined);
-      }),
-      map(actions => actions
-        .map(a => {
-          const data = a.payload.doc.data() as Client;
-          const id = a.payload.doc.id;
-          return { id, ...data } as ClientWithId;
-        })
-      ),
-      map(array => Array.from(new Set(array.map(c => c.id)))
-        .map(id => array.find(c => c.id === id))
-        // .sort((a, b) => a.name.first.localeCompare(b.name.first))
-      ),
-      map(data => {
-        data.map(c => {
-          // Add .url & .payed as placeholders for some observables
-          (c as any).url = this.service.getUrl(c.photo, c.sex);
-          (c as any).payed = this.service.hasPayed(c.pack.idSubscription);
-        });
-        return data;
-      }),
-      takeUntil(this.ngUnsubscribe)
-    ).subscribe(data => {
-      this.clients = data;
-      this.isLoading = false;
-    }, () => {
-      this.snack.open(this.i18n.nativeElement.childNodes[0].textContent, 'X', { duration: 2000 });
-      this.isLoading = false;
-    });
+      );
   }
 
   search(s: string) {
